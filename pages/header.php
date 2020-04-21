2ec8ca3630877d43223ab9a56210738bafb2211f
Michel48 <37518392+Michel48@users.noreply.github.com>
2020-04-21 22:13:01 +0000

    Add files via upload

---
+++ pages/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SCOLAR</title>
+    <!-------Font Awesome Icons-->
+    <link rel="stylesheet" href="./css/all.css">
+
+    <link rel="stylesheet" href="./css/font.css">
+    <!---Style Css-->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <link rel="stylesheet" href="./css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+
+</head>
+<body>
+
+    <!------------------Navigation----------------------->
+
+    <nav class="nav">
+        <div class="nav-menu flex-row">
+            <div class="nav-brand">
+                <a href="" class="text-gray">SCOLAR</a>
+            </div>
+            <div>
+                <ul class="nav-items">
+                    <li class="nav-link">
+                        <a href="#">Home</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">Cours</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
