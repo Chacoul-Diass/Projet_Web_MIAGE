bb21bb80b89a8f4150320b2f2c44359ded14d4e9
Michel48 <37518392+Michel48@users.noreply.github.com>
2020-04-21 22:09:21 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,196 @@
+<?php
+require('./pages/header.php');
+?>
+
+<div class="name-user">
+    <i class="fas fa-user"></i></a>
+</div>
+</div>
+</nav>
+<div>
+    <div class="header">
+        <div class="info-panel">
+            <a class="header-wrapper" href="#">
+                <div class="card panel-info1">
+                    <span class="panel-result">17</span>
+                    <i class="info-panel icon ametrre"><img src="./images/icon1.png" alt="" class="icon"> </i>
+                    <div class="panel-description">
+                        Cours Suvis
+                    </div>
+                </div>
+        </div>
+        <div class="info-panel">
+            <a class="header-wrapper" href="#">
+                <div class="card panel-info2">
+                    <span class="panel-result">17</span>
+                    <i class="info-panel icon ametrre"><img src="./images/icon2.png" alt="" class="icon"> </i>
+                    <div class="panel-description">
+                        Cours Suvis
+                    </div>
+                </div>
+        </div>
+        <div class="info-panel">
+            <a class="header-wrapper" href="#">
+                <div class="card panel-info3">
+                    <span class="panel-result">17</span>
+                    <i class="info-panel icon ametrre"><img src="./images/icon3.png" alt="" class="icon"> </i>
+                    <div class="panel-description">
+                        Cours Suvis
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+<div>
+    <div class="panel">
+        <div class="panel-tab">
+            <h2 class="Titre">Cours</h2>
+            <table id="liste-cours-suivi" class="dashboard-tab">
+                <thead>
+                    <tr class="tab-header">
+                        <th class="text-left">Cours</th>
+                        <th class="text-second">Inscription</th>
+                        <th class="text-prog">Progression</th>
+                        <th class="text-statut">Statut</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-left">
+                            <a href="" class="link">Decouvrer le framework</a>
+                        </td>
+                        <td class="text-second-info">
+                            10/02/2000
+                        </td>
+                        <td class="text-prog">
+                            80
+                        </td>
+                        <td class="text-statut">
+                            Terminer
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">
+                            <a href="" class="link">Decouvrer le framework</a>
+                        </td>
+                        <td class="text-second-info">
+                            10/02/2000
+                        </td>
+                        <td class="text-prog">
+                            80
+                        </td>
+                        <td class="text-statut">
+                            Terminer
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">
+                            <a href="" class="link">Decouvrer le framework</a>
+                        </td>
+                        <td class="text-second-info">
+                            10/02/2000
+                        </td>
+                        <td class="text-prog">
+                            80
+                        </td>
+                        <td class="text-statut">
+                            Terminer
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">
+                            <a href="" class="link">Decouvrer le framework</a>
+                        </td>
+                        <td class="text-second-info">
+                            10/02/2000
+                        </td>
+                        <td class="text-prog">
+                            80
+                        </td>
+                        <td class="text-statut">
+                            Terminer
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">
+                            <a href="" class="link">Decouvrer le framework</a>
+                        </td>
+                        <td class="text-second-info">
+                            10/02/2000
+                        </td>
+                        <td class="text-prog">
+                            80
+                        </td>
+                        <td class="text-statut">
+                            Terminer
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left">
+                            <a href="" class="link">Decouvrer le framework</a>
+                        </td>
+                        <td class="text-second-info">
+                            10/02/2000
+                        </td>
+                        <td class="text-prog">
+                            80
+                        </td>
+                        <td class="text-statut">
+                            Terminer
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<div>
+    <div class="panel">
+        <h2 class="Titre">Dernier Ajout</h2>
+        <div class="panel-cours">
+            <div class="owl-carousel owl-theme panel-cours">
+                <div class="panel-content">
+                    <img src="./images/bootstrap_logo.png" alt="cours-1">
+                    <div class="cours-title">
+                        <h3>Programation Python</h3>
+                        <button class="btn btn-panel">Voir Plus</button>
+                        <span>5 Heures</span>
+                    </div>
+                </div>
+                <div class="panel-content">
+                    <img src="./images/bootstrap_logo.png" alt="cours-2">
+                    <div class="cours-title">
+                        <h3>Découvrez Boostrap</h3>
+                        <button class="btn btn-panel">Voir Plus</button>
+                        <span>5 Heures</span>
+                    </div>
+                </div>
+                <div class="panel-content">
+                    <img src="./images/bootstrap_logo.png" alt="cours-3">
+                    <div class="cours-title">
+                        <h3>Apprenez à la programmation Javascript</h3>
+                        <button class="btn btn-panel">Voir Plus</button>
+                        <span>5 Heures</span>
+                    </div>
+                </div>
+                <div class="panel-content">
+                    <img src="./images/bootstrap_logo.png" alt="cours-4">
+                    <div class="cours-title">
+                        <h3>Decouvrez Le PHP</h3>
+                        <button class="btn btn-panel">Voir Plus</button>
+                        <span>5 Heures</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+   <?php 
+   require('./pages/footer.php');
+   ?>
+
+</div>
+
+
